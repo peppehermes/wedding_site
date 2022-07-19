1f56020f19ab090de8b6588dba04bd4ac797b444
Cory Moon <cory.moon@protonmail.com>
2022-07-19 14:59:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<div class="w-full text-center">
+    <h1 class="text-3xl font-bold underline text-purple-500 mb-8">Welcome to SvelteKit</h1>
+    <p class="font-mono">
+        Visit <a
+            class="text-purple-500 hover:text-purple-600 hover:underline"
+            href="https://kit.svelte.dev">kit.svelte.dev</a
+        > to read the documentation
+    </p>
+</div>
