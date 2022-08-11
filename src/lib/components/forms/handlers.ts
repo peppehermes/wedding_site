b028e03833aab3b6a86240e2836548a2fadd9f42
Cory Moon <cory.moon@protonmail.com>
2022-08-11 12:28:07 -0500

    switch to svelte-french-toast

--- src/lib/components/forms/handlers.ts
+++ src/lib/components/forms/handlers.ts
@@ -1,7 +1,28 @@
-import { showToast } from '$lib/stores/toast'
+// import { showToast } from '$lib/stores/toast'
 import { loading } from '$lib/stores/loading'
 import { toastMessages } from '$src/data/strings'
 import type { RsvpData } from '$lib/types'
+import toast from 'svelte-french-toast'
+
+const toastOptions: Record<string, unknown> = {
+    duration: 8000,
+    position: 'bottom-center',
+    style: 'border-radius: 9999px; background-color: #F6F3EF;',
+}
+
+const successIconTheme = {
+    iconTheme: {
+        primary: '#789ba4',
+        secondary: '#F6F3EF'
+    }
+}
+
+const errorIconTheme = {
+    iconTheme: {
+        primary: '#d48c97',
+        secondary: '#F6F3EF'
+    }
+}
 
 export const handleRsvpSubmit = async (data: RsvpData) => {
     loading.set(true)
@@ -13,12 +34,12 @@ export const handleRsvpSubmit = async (data: RsvpData) => {
         },
     })
         .then(() => {
-            showToast('success', toastMessages.rsvp.success)
+            toast.success(toastMessages.rsvp.success, {...toastOptions, ...successIconTheme})
             loading.set(false)
         })
         .catch((err) => {
             console.error('Error!', err.message)
-            showToast('error', toastMessages.rsvp.failure)
+            toast.error(toastMessages.rsvp.failure, {...toastOptions, ...errorIconTheme})
             loading.set(false)
         })
 }
@@ -33,12 +54,12 @@ export const handleEmailListSubmit = async (name: string, email: string) => {
         },
     })
         .then(() => {
-            showToast('success', toastMessages.email.success)
+            toast.success(toastMessages.email.success, {...toastOptions, ...successIconTheme})
             loading.set(false)
         })
         .catch((err) => {
             console.error('Error!', err.message)
-            showToast('error', toastMessages.email.failure)
+            toast.error(toastMessages.email.failure, {...toastOptions, ...errorIconTheme})
             loading.set(false)
         })
 }
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,9 +2,12 @@
     import '../styles/app.css'
     import Footer from '$lib/components/layout/Footer.svelte'
     import Toast from '$lib/components/elements/Toast.svelte';
+    import toast, { Toaster } from 'svelte-french-toast';
+
     
 </script>
 
+<Toaster />
 <Toast />
 <slot />
 <Footer />
