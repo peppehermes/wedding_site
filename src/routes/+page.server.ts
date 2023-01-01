50cdd054d5ad236f8a1594a622445b0cd576e8fa
Cory Moon <cory.moon@protonmail.com>
2023-01-01 12:09:26 -0600

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,32 +4,34 @@ import type { RsvpData } from '$lib/types'
 import type { RouteParams } from './$types'
 import type { RequestEvent } from '@sveltejs/kit'
 
-export const post = async ({ request }: RequestEvent<RouteParams>) => {
-    const config = configRepo.getConfig()
-    const json = await request.json()
+export const actions = {
+    default: async ({ request }: RequestEvent<RouteParams>) => {
+        const config = configRepo.getConfig()
+        const json = await request.json()
 
-    if (config.canRsvp) {
-        const data: RsvpData = {
-            email: json.data.email,
-            name: json.data.name,
-            guests: json.data.guests,
-            phone: json.data.phone,
-            transportation: json.data.transportation,
-            meals: json.data.meals,
-        }
+        if (config.canRsvp) {
+            const data: RsvpData = {
+                email: json.data.email,
+                name: json.data.name,
+                guests: json.data.guests,
+                phone: json.data.phone,
+                transportation: json.data.transportation,
+                meals: json.data.meals,
+            }
 
-        const res = await rsvpRepo.addToRsvpList(data)
-        if (res.object !== 'page') {
-            return {
-                status: 200,
+            const res = await rsvpRepo.addToRsvpList(data)
+            if (res.object !== 'page') {
+                return {
+                    status: 200,
+                }
+            }
+        } else {
+            const res = await rsvpRepo.addToEmailList(json.name, json.email)
+            if (res.object !== 'page') {
+                return {
+                    status: 200,
+                }
             }
         }
-    } else {
-        const res = await rsvpRepo.addToEmailList(json.name, json.email)
-        if (res.object !== 'page') {
-            return {
-                status: 200,
-            }
-        }
-    }
+    },
 }
