9a1dc02a53b1e75acb65229af29aa854fc1424f7
Giuseppe Mercurio <giuseppemercurio1997@gmail.com>
2023-06-11 16:14:49 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+    {
+        title: "Forum Login Service",
+        subtitle: "FastAPI Stack",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/project-1.gif",
+        link: "https://github.com/peppehermes/forum-login-service",
+    },
+    {
+        title: "SubitoBot",
+        subtitle: "Python bot",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/project-2.gif",
+        link: "https://github.com/peppehermes/subito_bot",
+    },
+    {
+        title: "SplinterCell",
+        subtitle: "Second-hand market",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/project-3.gif",
+        link: "https://bitbucket.org/breadsticksengineering/mad-lab2/src/master/",
+    },
+    {
+        title: "FBREF Scraper",
+        subtitle: "Football Database",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/project-4.gif",
+        link: "https://github.com/peppehermes/fbref-scraper",
+    },
+];
+
+export const skills = [
+    "Python",
+    "Kotlin",
+    "Android",
+    "Back-end",
+    "React",
+    "React Native"
+];
+
+export const testimonials = [
+    {
+        name: "Carla Melia",
+        company: "ORBYTA Tech",
+        image: "./img/carla.jpg",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?"
+    },
+    {
+        name: "Fabio Angelini",
+        company: "MDOTM",
+        image: "./img/fabio.jpg",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?"
+    },
+    {
+        name: "Gianpaolo Pirastu",
+        company: "Intesa Sanpaolo",
+        image: "./img/gianpaolo.jpg",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?"
+    },
+];
